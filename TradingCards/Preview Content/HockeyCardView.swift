642bf78acaa25ba0144806ffe3516015ec3f26ce
Hannu Packalen <hpackalen@lcs.on.ca>
2024-11-18 18:55:49 -0500

    Added the bottom half information.

--- TradingCards/Preview Content/HockeyCardView.swift
+++ TradingCards/Preview Content/HockeyCardView.swift
@@ -40,6 +40,25 @@ struct HockeyCardView: View {
             HStack{
                 VStack{
                     Text("Position: Winger")
+                    Text("Shoots: Right")
+                    Text("Points 23/24: 85")
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
+                    Text("Player signature")
+                    Image(.playerSignature)
+                        .resizable()
+                }
+                ZStack{
+                    RoundedRectangle(cornerRadius: 30)
+                        .fill(Color.white)
+                        .fill()
+                        .frame(width: 100, height: 100)
+                        .blur(radius: 10)
+                    VStack{
+                        Text("Fun Fact")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text("Marner was the second player (after Brad Richards in 2000) ever to win a Memorial Cup, a Stafford Smythe Memorial Trophy, CHL Player of the Year, a league MVP trophy (Red Tilson Trophy) and a league playoff MVP trophy (Wayne Gretzky 99 Award) in the same season.")
+                    }
                 }
             }
         }
